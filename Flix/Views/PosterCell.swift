1e3dca9d5ef42ecc962b931eeee708f85362b007
hamsikapongubala <hamsi.ruby@gmail.com>
2018-09-19 18:50:23 -0700

    Added Superhero tab

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Hamsika Pongubala on 9/18/18.
+//  Copyright © 2018 Hamsika Pongubala. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
